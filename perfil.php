700b6406ff4fba9bc6103b147c30a41ce0796094
IAteYourKookie <loorenachan@gmail.com>
2022-01-14 17:36:29 -0300

    ctrl f5

--- perfil.php
+++ perfil.php
@@ -3,7 +3,6 @@
   include_once('templates/header.php');
   ?>
 <!--------------------- Conteudo ---------------------->
-
   <div class="content">
       <center><div style="padding-top: 20px;">
       <img class="avatar" src="imagens/dog-g4c0a16c89_1920.jpg">
