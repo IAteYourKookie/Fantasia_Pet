f68378cd313f7a85f2ec46453f6d54da1e20886a
IAteYourKookie <loorenachan@gmail.com>
2022-01-18 20:52:25 -0300

    criação de paginas

--- index.php
+++ index.php
@@ -15,20 +15,19 @@
   <!--------------------- Fantasias --------------------->
       <div class="contentbody" id="produtos">
         <table><tr>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/elfo.png"><br>Fantasia Elfo<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/bombeiro.png"><br>Fantasia Bombeiro<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/reisapo.png"><br>Fantasia Rei Sapo<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/prisioneiro.png"><br>Fantasia Presidiario<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/elfo.png"><br>Fantasia Elfo<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/bombeiro.png"><br>Fantasia Bombeiro<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/reisapo.png"><br>Fantasia Rei Sapo<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/prisioneiro.png"><br>Fantasia Presidiario<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
         </tr>
         <tr>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/rebeldes.png"><br>Fantasia Escolar Rebeldes<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/indianajones.png"><br>Fantasia Indiana Jones<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/heroiamerica.png"><br>Fantasia Capitão America<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
-          <th><p class="box"><img class="boximgs" src="imagens/produtos/heroiaranha.png"><br>Fantasia Homem Aranha<br><button class="button" style="vertical-align:middle"><span>Veja mais</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/rebeldes.png"><br>Fantasia Escolar Rebeldes<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/indianajones.png"><br>Fantasia Indiana Jones<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/heroiamerica.png"><br>Fantasia Capitão America<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
+          <th><p class="box"><img class="boximgs" src="imagens/produtos/heroiaranha.png"><br>Fantasia Homem Aranha<br><button onclick="window.location.href='cart.php'" class="button" style="vertical-align:middle"><span>Comprar</span></button></p></th>
           </tr></table>
       </div>
       </div>
-
 <!----------------------- Footer ----------------------->
   <?php
   include('templates/footer.php');
